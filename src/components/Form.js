c91a8442d04462403d5f282f6ed2d86b73e630cf
circuit-monkey <p.ivcec@gmail.com>
2018-05-11 00:02:42 +0200

    start layout

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,13 +5,44 @@ class Form extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
+      vorname: '',
+      nachname: '',
+      mitgliedsname: '',
+      email: '',
+      passwort: '',
     };
   }
 
   render() {
     return (
-      <div className="form-container">
-        <button>Facebook</button>
+      <div className="container">
+        <div className="button-container">
+          <button className="facebook-button">Facebook</button>
+        </div>
+        <div className="headline">
+          <h2 class="background">
+            <span>
+              oder ohne facebook
+            </span>
+          </h2>
+        </div>
+        <div className="form-container">
+          <div className="input-container">
+            <input />
+          </div>
+          <div className="input-container">
+            <input />
+          </div>
+          <div className="input-container">
+            <input />
+          </div>
+          <div className="input-container">
+            <input />
+          </div>
+          <div className="input-container">
+            <input />
+          </div>
+        </div>
       </div>
     );
   }
